e3297e485116938d38d0fbfa81a1e6fbcf02bdbb
unknown <>
2017-11-30 23:17:20 +0800

    修改默认值

--- webapp/models.py
+++ webapp/models.py
@@ -86,7 +86,7 @@ class Post(db.Model):
     publish_date = db.Column(db.DateTime())
     update_date = db.Column(db.DateTime())
     post_hash = db.Column(db.String(255))
-    status = db.Column(db.Integer(),default=1)#状态
+    status = db.Column(db.Integer(),default=0)#状态
     comments = db.relationship('Comment', backref='post', lazy='dynamic')
     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
     tags = db.relationship('Tag', secondary=tags, backref=db.backref('posts', lazy='dynamic'))
